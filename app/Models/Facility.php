e051b6c49151214def1e698c98f5995b2781b7fe
glassesboyy <aliperwira26@gmail.com>
2025-04-10 14:41:47 +0700

    menyelesaikan semua code models dan migration, tinggal menjalankan migration ke database melalui command line

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+USE Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Facility extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'image',
+        'name',
+        'description',
+    ];
 }
--- app/Models/PromoCode.php
+++ app/Models/PromoCode.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PromoCode extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    
+    protected $fillable = [
+        'code',
+        'discount_type',
+        'discount',
+        'valid_until',
+        'is_used',
+    ];
 }

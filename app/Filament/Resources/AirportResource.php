12c96b0c9d5a3055263a549452e0ea043cdefeab
glassesboyy <aliperwira26@gmail.com>
2025-04-11 18:44:54 +0700

    CRUD Airports

---
+++ app/Filament/Resources/AirportResource.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AirportResource\Pages;
+use App\Filament\Resources\AirportResource\RelationManagers;
+use App\Models\Airport;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AirportResource extends Resource
+{
+    protected static ?string $model = Airport::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\FileUpload::make('image')
+                    ->image()
+                    ->directory('airports')
+                    ->required()
+                    ->columnSpan('full'),
+                Forms\Components\TextInput::make('iata_code')
+                    ->required()
+                    ->maxLength(3)
+                    ->unique(ignoreRecord: true),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('city')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('country')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('image')
+                    ->label('Image')
+                    ->size(50)
+                    ->disk('public')
+                    ->default('airports/default.png'),
+                Tables\Columns\TextColumn::make('iata_code')
+                    ->label('Iata Code')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('city')
+                    ->label('City')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('country')
+                    ->label('Country')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->sortable()
+                    ->dateTime()
+                    ->default(now()),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Updated At')
+                    ->sortable()
+                    ->dateTime()
+                    ->default(now()),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAirports::route('/'),
+            'create' => Pages\CreateAirport::route('/create'),
+            'edit' => Pages\EditAirport::route('/{record}/edit'),
+        ];
+    }
+}
